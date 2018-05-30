b05018e12f1c4e9619ca0ccfde7aeeee498aca8a
董重 <zhong.dong@flyingwings.cn>
2018-05-30 16:55:40 +0800

    悬浮Button

---
+++ app/src/main/java/dongzhong/testforfloatingwindow/FloatingWindowService.java
@@ -0,0 +1,101 @@
+package dongzhong.testforfloatingwindow;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.os.Build;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+/**
+ * Created by dongzhong on 2018/5/30.
+ */
+
+public class FloatingWindowService extends Service {
+    private WindowManager windowManager;
+    private WindowManager.LayoutParams layoutParams;
+
+    private Button button;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        layoutParams = new WindowManager.LayoutParams();
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+            layoutParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        } else {
+            layoutParams.type = WindowManager.LayoutParams.TYPE_PHONE;
+        }
+        layoutParams.format = PixelFormat.RGBA_8888;
+        layoutParams.gravity = Gravity.LEFT | Gravity.TOP;
+        layoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+        layoutParams.width = 100;
+        layoutParams.height = 100;
+        layoutParams.x = 300;
+        layoutParams.y = 300;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        showFloatingWindow();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void showFloatingWindow() {
+        button = new Button(getApplicationContext());
+        if (Settings.canDrawOverlays(this)) {
+            button.setText("Floating Window");
+            button.setBackgroundColor(Color.BLUE);
+            button.setWidth(100);
+            button.setHeight(100);
+            windowManager.addView(button, layoutParams);
+
+            button.setOnTouchListener(new FloatingOnTouchListener());
+        }
+    }
+
+    private class FloatingOnTouchListener implements View.OnTouchListener {
+        private int x;
+        private int y;
+
+        @Override
+        public boolean onTouch(View view, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    x = (int) event.getRawX();
+                    y = (int) event.getRawY();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    int nowX = (int) event.getRawX();
+                    int nowY = (int) event.getRawY();
+                    int movedX = nowX - x;
+                    int movedY = nowY - y;
+                    Log.d("悬浮窗", "movedX = " + movedX + ", movedY =" + movedY);
+                    x = nowX;
+                    y = nowY;
+                    layoutParams.x = layoutParams.x + movedX;
+                    layoutParams.y = layoutParams.y + movedY;
+                    windowManager.updateViewLayout(button, layoutParams);
+                    break;
+                default:
+                    break;
+            }
+            return false;
+        }
+    }
+}
---
+++ app/src/main/java/dongzhong/testforfloatingwindow/MainActivity.java
@@ -0,0 +1,39 @@
+package dongzhong.testforfloatingwindow;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1111) {
+            if (!Settings.canDrawOverlays(this)) {
+                Toast.makeText(this, "授权失败", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "授权成功", Toast.LENGTH_SHORT).show();
+                startService(new Intent(MainActivity.this, FloatingWindowService.class));
+            }
+        }
+    }
+
+    public void startFloatingService(View view) {
+        if (!Settings.canDrawOverlays(this)) {
+            Toast.makeText(this, "当前无权限，请授权", Toast.LENGTH_SHORT);
+            startActivityForResult(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName())), 1111);
+        } else {
+            startService(new Intent(MainActivity.this, FloatingWindowService.class));
+        }
+    }
+}
